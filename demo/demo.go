2eef5b9bc826e918ce85c74690c0a884c31ed9dc
qwj <wjqing@landhightech.com>
2019-10-25 17:33:45 +0800

    增加demo

---
+++ demo/demo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"gpool"
+	"time"
+)
+
+func main() {
+	gp := gpool.New(2)
+	for i := 1; i <= 100; i++ {
+		gp.Add(1)
+		go func(j int) {
+			defer gp.Done()
+			fmt.Println(j)
+			time.Sleep(1 * time.Second)
+		}(i)
+	}
+	gp.Wait()
+}
